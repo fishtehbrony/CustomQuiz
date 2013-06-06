6cc66015b119eec0658bbce57857eb1d219973bb
fishtehbrony <fishii10@me.com>
2013-06-06 15:58:19 +0200

    V1.0EX
    Fixed so that answers are written first. This config is now bug-free and fully compatible!

--- quiz_config.py
+++ quiz_config.py
@@ -1,5 +1,5 @@
 print("Welcome to quiz_config")
-print("You can exit the config at any time with Alt+F4.")
+print("You can exit the config AS LONG AS THE REQUEST IS 'Please enter the question you'd like to ask:' with Alt+F4.")
 title = input("Please enter the subject:")
 meh = "Z:/Documents/Quizzes/" + str(title)
 print(meh)          #Change 'meh' according to where you want to put your quizzes
@@ -9,7 +9,7 @@ f.close
 while True:
     f = open(meh + ".txt", "a")
     question = str(input("Please enter the question you'd like to ask:"))
-    f.write(str(question) + ("\n"))
     answer = str(input("Please enter the correct answer to that question:"))
     f.write(str(answer) + ("\n"))
+    f.write(str(question) + ("\n"))
     f.close()
